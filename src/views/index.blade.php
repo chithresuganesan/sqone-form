6139337fee571a9f698c1fa79ddf4c6a83d980c0
chithresuganesan <chithresu@secqureone.com>
2021-05-08 12:44:10 +0000

    button color change

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -4,12 +4,12 @@
     <div class="container">
         <div class="card text-center">
             <div class="card-header">
-              Sample Contact Form - {{ config('form.phone') }}
+              Sample Contact Form  - {{ config('form.phone') }}
             </div>
             <div class="card-body">
               <h5 class="card-title">Special title treatment</h5>
               <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="{{ route('contact') }}" class="btn btn-primary">Go Create</a>
+              <a href="{{ route('contact') }}" class="btn btn-success">Go Create</a>
               <a href="{{ route('show') }}" class="btn btn-primary">Go visit</a>
             </div>
             <div class="card-footer text-muted">
